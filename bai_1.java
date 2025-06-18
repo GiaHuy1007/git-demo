738f6160ee1b0b9bb7ae214dd4c788645ef54927
Gia Huy <giahuy10072006@gmail.com>
2025-06-18 13:29:59 +0700

    test nha!

---
+++ bai_1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class bai_1 {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap a: ");
+        a = sc.nextInt();
+        System.out.print("Nhap b: ");
+        b = sc.nextInt();
+        System.out.print("Nhap c: ");
+        c = sc.nextInt();
+        System.out.print("Gia tri cua a, b, c lan luot la: ");
+        System.out.print(a + " " + b + " " + c);
+    }
+}
